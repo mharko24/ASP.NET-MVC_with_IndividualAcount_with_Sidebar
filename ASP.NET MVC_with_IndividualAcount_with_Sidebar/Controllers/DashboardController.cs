385f10ae048944074c6587821be3543985189cd5
mhark cagampan <mharkcagampan@gmail.com>
2024-12-20 08:51:55 +0800

    Add project files.

---
+++ ASP.NET MVC_with_IndividualAcount_with_Sidebar/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_MVC_with_IndividualAcount_with_Sidebar.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASP.NET MVC_with_IndividualAcount_with_Sidebar/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.NET_MVC_with_IndividualAcount_with_Sidebar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
